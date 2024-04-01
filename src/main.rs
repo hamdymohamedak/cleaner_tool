58567fe060d633c7e703bf3edd2a5cabcfc77d4d
hamdymohamedak <mohameddhamdy407@gmail.com>
2024-04-01 09:13:07 +0200

    first commit

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::fs;
+use std::io;
+use std::io::Write;
+
+fn main() {
+    println!("Please enter the directory path:");
+    let mut text_input = String::new();
+    io::stdin()
+        .read_line(&mut text_input)
+        .expect("Failed to read line");
+    let path_dir = text_input.trim();
+
+    if let Err(err) = remove_empty_directories(path_dir) {
+        println!("Error: {}", err);
+    } else {
+        println!("Empty directories removed successfully.");
+    }
+}
+
+fn remove_empty_directories(dir_path: &str) -> io::Result<()> {
+    visit_dirs(dir_path, &|entry| {
+        if entry.path().is_dir() {
+            let sub_dir_path = entry.path();
+            visit_dirs(sub_dir_path.to_str().unwrap(), &|_| Ok(()))?;
+            if is_empty_directory(&sub_dir_path)? {
+                print!("Removing directory '{}'... ", sub_dir_path.display());
+                io::stdout().flush().unwrap();
+                fs::remove_dir(sub_dir_path)?;
+                println!("Done");
+            }
+        }
+        Ok(())
+    })
+}
+
+fn visit_dirs(dir_path: &str, cb: &dyn Fn(fs::DirEntry) -> io::Result<()>) -> io::Result<()> {
+    let dir_entries = fs::read_dir(dir_path)?;
+    for entry in dir_entries {
+        let entry = entry?;
+        cb(entry)?;
+    }
+    Ok(())
+}
+
+fn is_empty_directory(dir_path: &std::path::Path) -> io::Result<bool> {
+    let mut entries = fs::read_dir(dir_path)?;
+    Ok(entries.next().is_none())
+}
